2faabee182718bcb24a8f038dfa559c84f01cb82
Soban Farooq <sobanfarooq3@gmail.com>
2026-06-08 18:47:59 +0500

    Set up package foundation

---
+++ config/palette.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'count' => 5,
+
+    'method' => 'representative',
+
+    'background' => null,
+
+    'cache' => [
+        'enabled' => false,
+        'ttl' => 60 * 24,
+        'prefix' => 'palette:',
+    ],
+
+    'disk' => null,
+];
